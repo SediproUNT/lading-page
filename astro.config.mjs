c623ce5cd439f3b455d0d2f8570ea1bba2f9f7c9
Rommel Ulco <rulco@unitru.edu.pe>
2024-12-04 02:00:53 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    site: "https://sediprount.github.io/",
-    base: "lading-page",
-
-
     integrations: [
         tailwind()
     ],
